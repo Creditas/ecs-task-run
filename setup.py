57fa634afd9acc3c6beca88a5bb62e943b569894
Aurelio Saraiva <aureliosaraiva@gmail.com>
2018-07-19 15:09:36 -0300

    added base

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ecs_task_run",
+    version="0.0.1",
+    author="Aurelio Saraiva",
+    author_email="aurelio.saraiva@creditas.com.br",
+    description="ECS Task Definition run",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/creditas/ecs-task-run",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+
+    ]
+)
